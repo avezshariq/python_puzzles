8a4e3b9f652ad06de381086e921cdc9cd2c36261
avezshariq <avezshariq@gmail.com>
2022-02-03 13:08:33 +0530

    started solution
    but th solution does not work yet. It only returns the original number

--- reverse_the_number.py
+++ reverse_the_number.py
@@ -3,3 +3,8 @@
 
 # Example: User input = 123456
 #          print      = 654321
+
+print('This program takes an input from the user and reverse prints it')
+print('Example, the user gives - 123456, then the program prints - 654321')
+num = int(input('Enter a number :'))
+print(num)
