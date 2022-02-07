3bcbfc0599b638f37210c9b3ea01f325519fe610
avezshariq <avezshariq@gmail.com>
2022-02-07 21:42:55 +0530

    added steps pattern file

---
+++ steps_pattern.py
@@ -0,0 +1,15 @@
+# Simple Steps pattern
+
+n = 15
+for ind in range(1,n):
+	if ind % 2 == 0:
+		print(' ' * (ind-1), end = '')
+		print('|')
+	else:
+		print(' ' * (ind-1), end = '')
+		print('_')
+
+print('OR, IF YOU PREFER IT THIS WAY')
+for ind in range(1,n):
+	print(' ' * (ind-1), end = '')
+	print(7)
